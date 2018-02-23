275e1321a773cb8bcf9159ceaa7db25ca325da90
Sheng Yunzhou <shengyunzhou@gmail.com>
2018-02-23 16:51:31 +0800

    Complete till No. 28

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -2,7 +2,9 @@ package iii_conventions
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange {
+    return DateRange(this, other)
+}
 
 enum class TimeInterval {
     DAY,
@@ -11,3 +13,21 @@ enum class TimeInterval {
 }
 
 class DateRange(val start: MyDate, val endInclusive: MyDate)
+
+operator fun DateRange.iterator(): Iterator<MyDate> {
+    return DateIterator(this)
+}
+
+class DateIterator(val dateRange: DateRange) : Iterator<MyDate> {
+    var current = dateRange.start
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.addTimeIntervals(TimeInterval.DAY, 1)
+        return result
+    }
+
+    override fun hasNext(): Boolean {
+        return current <= dateRange.endInclusive
+    }
+}
--- src/iii_conventions/n25Comparison.kt
+++ src/iii_conventions/n25Comparison.kt
@@ -16,7 +16,15 @@ fun todoTask25(): Nothing = TODO(
 )
 
 fun task25(date1: MyDate, date2: MyDate): Boolean {
-    todoTask25()
-//    return date1 < date2
+//    todoTask25()
+    return date1 < date2
+}
+
+operator fun MyDate.compareTo(date2: MyDate): Int {
+    if (year < date2.year) return -1
+    if (year == date2.year && month < date2.month) return -1
+    if (year == date2.year && month == date2.month && dayOfMonth < date2.dayOfMonth) return -1
+    if (year == date2.year && month == date2.month && dayOfMonth == date2.dayOfMonth) return 0
+    return 1
 }
 
